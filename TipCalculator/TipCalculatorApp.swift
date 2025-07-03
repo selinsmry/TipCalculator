719904943565ce85f2a53dfcb4cb38388cf598c4
selinsmry <selinsamray12@gmail.com>
2025-07-03 14:12:54 +0300

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Selin Samray on 3.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorTests.swift
+//  TipCalculatorTests
+//
+//  Created by Selin Samray on 3.07.2025.
+//
+
+import Testing
+@testable import TipCalculator
+
+struct TipCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
